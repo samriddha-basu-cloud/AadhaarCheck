3ffd9ff6eb78940bbf72738762ceb24208c429a9
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2024-06-20 22:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 import requests
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-
 CORS(app)
 
 @app.route('/')
@@ -61,4 +61,5 @@ def verify_aadhaar():
     return jsonify(verify_uid_response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
